04edc802b1dad89f4816c27a30abda9415d3a50a
Jude Beadle <info@jbeadle.de>
2018-08-18 16:38:11 +0200

    feat: letting the react router handle switch between default and search

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { Component } from 'react'
-// import { Route } from 'react-router-dom'
-import ListBooks from './ListBooks'
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import ListBooks from './ListBooks'
+import SearchBooks from './SearchBooks'
 import './App.css'
 
 class BooksApp extends React.Component {
+  
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    //showSearchPage: false
     books: []
   }
+
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books })
@@ -24,35 +20,20 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
 
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-
-
-        ) : (
-          <ListBooks 
-              books={this.state.books}
+        <Route exact path="/" render={() => (
+          <ListBooks
+            // TODO function for switching shelf
+            // onChangeShelf={this.changeShelf} 
+            books={this.state.books}
           />
-        )}
+        )} />
+        <Route path="/search" render={({ history }) => (
+          <SearchBooks
+            // this.state.showSearchPage
+            // books={this.state.books}
+          />
+        )} />
       </div>
     )
   }
